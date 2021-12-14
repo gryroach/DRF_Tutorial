83e1b8af6ca561901cce79803d96b18b64951bf5
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-14 11:28:54 +0300

    Part 5. Relationships & Hyperlinked APIs

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view())
+    path('snippets/', views.SnippetList.as_view(), name='snippets-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('', views.api_root),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight')
 ]
 # Adding optional format suffixes
 urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tutorial.urls'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 3
 }
 
 TEMPLATES = [
